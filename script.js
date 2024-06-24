345c5e947e3ac3cce7ae8b382a9675338ed8e02c
Adam Baker <148913138+arbaker82@users.noreply.github.com>
2024-06-24 13:23:26 -0400

    3 button synth

--- script.js
+++ script.js
@@ -1,12 +1,44 @@
-document.querySelector('#play').addEventListener('click', () => {
-    const AudioContext = window.AudioContext || window.webkitAudioContext;
-    const actx = new AudioContext();
-    if (!actx) throw 'AudioContext not supported :(';
+let synth, reverb;
 
-    const osc = actx.createOscillator();
-    osc.type = 'sawtooth';
-    osc.frequency.value = 440;
-    osc.connect(actx.destination);
-    osc.start();
-    osc.stop(actx.currentTime + 2);
-});
+function preload(){
+    synth = new Tone.Synth({
+        oscillator: {
+            type: "pwm",
+        },
+    });
+
+    reverb = new Tone.JCReverb(0.5);
+}
+
+function setup() {
+    createCanvas(400, 400);
+    synth.chain(reverb, Tone.Destination);
+
+    let buttonA4 = createButton('A4');
+    buttonA4.position(100, 100);
+    buttonA4.mousePressed(playA4);
+
+    let buttonB4 = createButton('B4');
+    buttonB4.position(200, 100);
+    buttonB4.mousePressed(playB4);
+
+    let buttonE5 = createButton('E5');
+    buttonE5.position(300, 100);
+    buttonE5.mousePressed(playE5);
+}
+
+function draw() {
+    background(220);
+}
+
+function playA4() {
+    synth.triggerAttackRelease("A4", 0.5);
+}
+
+function playB4() {
+    synth.triggerAttackRelease("B4", 0.5);
+}
+
+function playE5() {
+    synth.triggerAttackRelease("E5", 0.5);
+}
